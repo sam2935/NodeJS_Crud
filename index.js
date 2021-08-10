2f3e31559d50d575258b0455c75dc29aed23d506
shyam ji mishra <36323489+sam2935@users.noreply.github.com>
2021-08-10 14:13:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,78 +1,35 @@
-//use path module
-const path = require('path');
-//use express module
-const express = require('express');
-//use hbs view engine
-const hbs = require('hbs');
-//use bodyParser middleware
-const bodyParser = require('body-parser');
-//use mysql database
-const mysql = require('mysql');
-const app = express();
- 
-//Create connection
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'crud_db'
-});
- 
-//connect to database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
-});
- 
-//set views file
-app.set('views',path.join(__dirname,'views'));
-//set view engine
-app.set('view engine', 'hbs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-//set public folder as static folder for static file
-app.use('/assets',express.static(__dirname + '/public'));
- 
-//route for homepage
-app.get('/',(req, res) => {
-  let sql = "SELECT * FROM product";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.render('product_view',{
-      results: results
-    });
-  });
-});
- 
-//route for insert data
-app.post('/save',(req, res) => {
-  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
-  let sql = "INSERT INTO product SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
-});
- 
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
-});
- 
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
-  });
-});
- 
-//server listening
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
-});
+const puppeteer = require('puppeteer');
+try {
+    (async () => {
+        /** by default puppeteer launch method have headless option true*/
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com/');
+        await page.type('input[aria-label="Search"]', 'node js tutorial');
+        await page.keyboard.press('Enter');
+        // await page.waitForNavigation(),
+        // page.click('a[href="https://www.w3schools.com/nodejs/"]');
+
+        /** waitfor while loding the page, otherwise evaulate method will get failed. */
+         await page.waitForTimeout(5000);
+         const list = await page.evaluate(() => {
+             let data = []
+             /** this can be changed for other website.*/
+             const list = document.querySelectorAll('.yuRUbf');
+             for (const a of list) {
+                 data.push({
+                     'title': a.querySelector('.LC20lb').textContent ?? null,
+                     'link': a.querySelector('a').href
+                 })
+              }
+            
+             return data;
+         })
+         console.log(list);
+         await browser.close();
+    })()
+}catch (err) {
+    console.error(err);
+}
